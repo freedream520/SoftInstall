18c1b9b650e37c661680d35646bf6f95629ae003
745580334 <liush20151231@sina.com>
2016-04-01 10:31:05 +0800

    Update drbd-install.sh

--- drbd/drbd-install.sh
+++ drbd/drbd-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ############### ENV ##############
 #         disk /dev/sdb1         #
-# hostname	node1	172.16.0.183 #
-# hostname	node2	172.16.0.184 #
+# hostname  node1   172.16.0.183 #
+# hostname  node2   172.16.0.184 #
 ##################################
 SOFT_DIR=/usr/local/src/
 DRBD=drbd-8.4.1.tar.gz
@@ -49,8 +49,8 @@ set_drbd() {
 			protocol	C;#使用drbd同步协议
 			handlers {
 				pri-on-incon-degr "/usr/lib/drbd/notify-pri-on-incon-degr.sh; /usr/lib/drbd/notify-emergency-reboot.sh; echo b > /proc/sysrq-trigger ; reboot -f";
-		        pri-lost-after-sb "/usr/lib/drbd/notify-pri-lost-after-sb.sh; /usr/lib/drbd/notify-emergency-reboot.sh; echo b > /proc/sysrq-trigger ; reboot -f";
-		        local-io-error "/usr/lib/drbd/notify-io-error.sh; /usr/lib/drbd/notify-emergency-shutdown.sh; echo o > /proc/sysrq-trigger ; halt -f"; 
+		        	pri-lost-after-sb "/usr/lib/drbd/notify-pri-lost-after-sb.sh; /usr/lib/drbd/notify-emergency-reboot.sh; echo b > /proc/sysrq-trigger ; reboot -f";
+		        	local-io-error "/usr/lib/drbd/notify-io-error.sh; /usr/lib/drbd/notify-emergency-shutdown.sh; echo o > /proc/sysrq-trigger ; halt -f"; 
 			}
 			startup {
 				#wfc-timeout degr-wfc-timeout outdated-wfc-timeout wait-after-sb 
@@ -59,11 +59,11 @@ set_drbd() {
 				#cpu-mask on-no-data-accessible
 			}
 			disk {
-				on-io-error		detach;#配置I/O错误处理策略为分离
+				on-io-error	detach;#配置I/O错误处理策略为分离
 			}
 			net {
 				cram-hmac-alg	"sha1";#设置加密算法
-        		shared-secret 	"allendrbd";#设置加密密钥 
+        			shared-secret 	"allendrbd";#设置加密密钥 
 			}
 			syncer {
 				rate	1024M;#设置主备节点同步时的网络速率
@@ -71,7 +71,7 @@ set_drbd() {
 		}
 	}
 	#添加资源文件
-	vim	drbd.d/drbd.res
+	vim drbd.d/drbd.res
 	resource drbd {
 		on node1 {
 			device		/dev/drbd0;
@@ -94,17 +94,17 @@ startup() {
 	service drbd start
 	#set in primary(node1)
 	drbdadm primary --force drbd 	#设置主节点
-	cat /proc/drbd 					#查看节点状态
+	cat /proc/drbd 			#查看节点状态
 	mkfs.ext3 /dev/drbd0
 	mount /dev/drbd0 /db
 }
 order() {
-	service drbd start 			#启动drbd
-	service drbd stop 			#停止drbd
-	drbdadm up drbd 			#启动drbd
+	service drbd start 		#启动drbd
+	service drbd stop 		#停止drbd
+	drbdadm up drbd 		#启动drbd
 	drbdadm cstate drbd 		#查看资源连接状态
-	drdbadm down drbd 			#停止drbd
-	drbdadm role drbd 			#查看资源角色
+	drdbadm down drbd 		#停止drbd
+	drbdadm role drbd 		#查看资源角色
 	drbdadm dstate drbd 		#查看硬盘状态
 	drbdadm primary drbd 		#升为主节点
 	drbdadm secondary drbd 		#降为备节点
@@ -112,5 +112,5 @@ order() {
 
 drbd说明：
 
-只有主节点才能挂载/dev/drbd0,备节点不可挂载。备节点要挂载/dev/drbd0，必须先升级为主节点，同时原主节点必须降级为备节点。
+    只有主节点才能挂载/dev/drbd0,备节点不可挂载。备节点要挂载/dev/drbd0，必须先升级为主节点，同时原主节点必须降级为备节点。
 当需要查看备节点同步主节点的数据是否成功时，可以先在备节点运行service drbd stop，然后挂载/dev/sdb1查看数据。
